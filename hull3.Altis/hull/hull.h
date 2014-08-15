bb22718b6a456db09c165ac3539348108735c302
László Major <laszlo.major@outlook.com>
2014-08-15 19:16:45 +0200

    Added sample mission.

---
+++ hull3.Altis/hull/hull.h
@@ -0,0 +1,15 @@
+class Hull {
+    isEnabled = 1;
+
+    class Gear {
+        factions[] = {
+        };
+    };
+
+    class Briefing {
+        blufor = "hull\briefing\blufor.sqf";
+        opfor = "hull\briefing\opfor.sqf";
+        indfor = "hull\briefing\indfor.sqf";
+        civilian = "hull\briefing\civilian.sqf";
+    };
+};
