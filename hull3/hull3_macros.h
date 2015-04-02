4e86e2cb282203ac1ece2091e284de3315e4b72e
László Major <laszlo.major@outlook.com>
2015-04-03 00:01:18 +0200

    Updated version to v0.3.1 .

--- hull3/hull3_macros.h
+++ hull3/hull3_macros.h
@@ -3,7 +3,7 @@
 
 #include "\userconfig\hull3\log\global.h"
 
-#define HULL3_VERSION                    "0.3.0"
+#define HULL3_VERSION                    "0.3.1"
 
 #define CONCAT_ADDON_PATH(FILE)         x\ark\addons\hull3\FILE
 #define ADDON_PATH(FILE)                #CONCAT_ADDON_PATH(FILE)
