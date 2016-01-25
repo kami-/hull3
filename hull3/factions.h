7611dc5b0273a2bfe7b12d0b63bcc26e79c6f958
SatireFA <cwood686@gmail.com>
2016-01-25 21:58:57 +0000

    Adding name & descriptions

--- hull3/factions.h
+++ hull3/factions.h
@@ -9,495 +9,505 @@ class Faction {
     class BLU_F {
         gear = "MX_NATO";
         uniform = "NATO";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "NATO";
+        description = "NATO Multicam with MX Series Weapons";
+        languages[] = {{"west", 100}};
     };
 
     class BLU_G_F {
         gear = "TRG_FIA";
         uniform = "FIA";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "FIA";
+        description = "FIA Rebels with TRG Series Weapons";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_USMC_WD {
         gear = "M16A4_USMC";
         uniform = "USMC_WD";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "USMC Woodland";
+        description = "Woodland MARPAT with M16A4's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_USMC_DE {
         gear = "M16A4_USMC";
         uniform = "USMC_DE";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "USMC Desert";
+        description = "Desert MARPAT with M16A4's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_USMC_SN {
         gear = "M16A4_USMC";
         uniform = "USMC_SN";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "USMC Snow";
+        description = "Snow Camo with M16A4's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_US_RAN {
         gear = "SCAR_L_US";
         uniform = "USA_RAN";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "US Rangers (ACU)";
+        description = "ACU uniforms with SCAR-L";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_US_ACU {
         gear = "M4_US";
         uniform = "USA_ACU";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "US Army ACU";
+        description = "ACU uniforms with M4's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_US_WD {
         gear = "M16A2_US";
         uniform = "USA_WD";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "US Army Woodland";
+        description = "Woodland uniforms with M16A2";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_US_DE {
         gear = "M16A2_US";
         uniform = "USA_DE";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "US Army Desert";
+        description = "3 Colour Desert uniforms with M16A2";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_US_RAN_SCO {
         gear = "SCAR_H_US";
         uniform = "USA_RAN_SCO";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "US Rangers (Scorpion)";
+        description = "Scorpion uniforms with SCAR-H";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_US_SCO {
         gear = "M4A1_US";
         uniform = "USA_SCO";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "US Army Scorpion";
+        description = "Scorpion Multicam uniforms with M4A1's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_US_MC {
         gear = "M4A1_US";
         uniform = "USA_MC";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "US Army Multicam";
+        description = "Multicam uniforms with M4A1's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_US_MC_DRK {
         gear = "M4A1_US";
         uniform = "USA_MC_DRK";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "US Army Dark Multicam";
+        description = "Drak Green Multicam uniforms with M4A1's";
+        languages[] = {{"west", 100}};
     };
-    
+
     class MNP_GE_WD {
         gear = "HK416";
         uniform = "GER_WD";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "German Army Woodland";
+        description = "Flecktarn uniforms with HK416's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_GE_DE {
         gear = "HK416";
         uniform = "GER_DE";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "German Army Desert";
+        description = "Tropentarn uniforms with HK416's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_CA_WD {
         gear = "M16A4_CAN";
         uniform = "CAN_WD";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "Canadian Army Woodland";
+        description = "Woodland CADPAT uniforms with M16A4's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_CA_DE {
         gear = "M16A4_CAN";
         uniform = "CAN_DE";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "Canadian Army Desert";
+        description = "Desert CADPAT uniforms with M16A4's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_AUS {
         gear = "AUGA3_AUS";
         uniform = "AUS_WD";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "Australian Army";
+        description = "Auscam uniforms with AUG A3's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_AUS_AMCU {
         gear = "AUGA3_AUS";
         uniform = "AUS_AMCU";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "Australian Army AMCU";
+        description = "AMCU uniforms with AUG A3's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_ROK {
         gear = "M16A4_ROK";
         uniform = "ROK_WD";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "Republic of Korea";
+        description = "Digital Woodland uniforms with M16A4's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_UKR_TTSKO {
         gear = "AK74_UKR";
         uniform = "UKR_TTSKO";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "Ukranian Army";
+        description = "Woodland TTSKO uniforms and AK-74's";
+        languages[] = {{"west", 100}};
     };
 
     class MNP_PMC {
         gear = "FAL_PMC";
         uniform = "PMC_OD";
-        languages[] = {
-            {"west", 100}
-        };
+        name = "PMC (OD)";
+        description = "Olive Drab uniforms and FAL's";
+        languages[] = {{"west", 100}};
     };
 
     class OPF_F {
         gear = "Katiba_CSAT";
         uniform = "CSAT";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "CSAT";
+        description = "CSAT Uniforms and Katiba";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_OPF_F_SN {
         gear = "Katiba_CSAT";
         uniform = "CSAT_SN";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "CSAT Snow";
+        description = "Snow CSAT Uniforms and Katiba";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_CH_WD {
         gear = "AK74_CN";
         uniform = "CN_WD";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "PLA Universal";
+        description = "Chinese Universal Uniforms and AK-74's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_CH_WD2 {
         gear = "AK74_CN";
         uniform = "CN_WD2";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "PLA Woodland";
+        description = "Chinese Woodland Uniforms and AK-74's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_CH_DE {
         gear = "AK74_CN";
         uniform = "CN_DE";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "PLA Desert";
+        description = "Chinese Desert Uniforms and AK-74's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_CH_MAR {
         gear = "AKS74_CN";
         uniform = "CN_MAR";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "PLA Universal";
+        description = "Chinese Marine Uniforms and AK-74's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_CH_HWD {
         gear = "Katiba_CN";
         uniform = "CN_H_WD";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "PLA Universal (Heavy)";
+        description = "CSAT Style Chinese Universal Uniforms and Katiba";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_CH_HDE {
         gear = "Katiba_CN";
         uniform = "CN_H_DE";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "PLA Desert (Heavy)";
+        description = "CSAT Style Chinese Desert Uniforms and Katiba";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_RU_VDV_WD {
         gear = "AK12_RU";
         uniform = "RU_WD_DIG";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Russian VDV Woodland";
+        description = "Digital Woodland Uniforms and AK-12's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_RU_VDV_DE {
         gear = "AK12_RU";
         uniform = "RU_DE";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Russian VDV Desert";
+        description = "Digital Desert Uniforms and AK-12's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_RU_HWD {
         gear = "AK12_RU";
         uniform = "RU_H_WD";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Russian Heavy Woodland";
+        description = "CSAT Style Digital Woodland Uniforms and AK-12's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_RU_HDE {
         gear = "AK12_RU";
         uniform = "RU_H_DE";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Russian Heavy Desert";
+        description = "CSAT Style Digital Desert Uniforms and AK-12's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_RU_WD {
         gear = "AK74_RU";
         uniform = "RU_WD_FL";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Russian Woodland (Flora)";
+        description = "Woodland Flora Uniforms and AK-74's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_RU_TTK {
         gear = "AK74_RU";
         uniform = "RU_WD_TT";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Russian Woodland (TTSkO";
+        description = "Woodland TTSkO Uniforms and AK-74's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_RU_AIR {
         gear = "AKS74_RU";
         uniform = "RU_AB";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Soviet Airborne";
+        description = "Afghan War era camo suits & AK-74's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_RU_REED {
         gear = "AEK_RU";
         uniform = "RU_REED";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Russian MVD Woodland";
+        description = "Woodland Reed Uniforms and AEK's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_KOR_OD {
         gear = "AK47_NK";
         uniform = "NK_CON";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "North Korean Conscripts";
+        description = "Olive Drab Uniform and AK-47's";
+        languages[] = {{"east", 100}};
+    };
+
+    class MNP_KOR_WD {
+        gear = "AK74_NK";
+        uniform = "NK_REG";
+        name = "North Korean Regulars";
+        description = "Woodland Uniform and AK-74's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_IRN_WD {
         gear = "G3A4_IRN";
         uniform = "IRN_WD";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Iranian Army Woodland";
+        description = "Woodland Uniform and G3A3's";
+        languages[] = {{"east", 100}};
     };
+
     class MNP_IRN_RG_WD {
         gear = "AKM_IRN";
         uniform = "IRN_RG_WD";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Iranian Revolutionary Guard Woodland";
+        description = "Woodland Militia Uniform and AKM's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_IRN_QF_WD {
         gear = "Katiba_IRN";
         uniform = "IRN_QF_WD";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Iranian Quds Force Woodland";
+        description = "Woodland SF Uniform and Katiba";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_IRN_DE {
         gear = "G3A4_IRN";
         uniform = "IRN_DE";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Iranian Army Desert";
+        description = "Desert Uniform and G3A3's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_IRN_RG_DE {
         gear = "AKM_IRN";
         uniform = "IRN_RG_DE";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Iranian Revolutionary Guard Desert";
+        description = "Desert Militia Uniform and AKM's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_IRN_QF_DE {
         gear = "Katiba_IRN";
         uniform = "IRN_QF_DE";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Iranian Quds Force Desert";
+        description = "Desert SF Uniform and Katiba";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_DPR_SUM {
         gear = "AK74_DPR";
         uniform = "DPR_SUM";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Donetsk People's Republic (Summer)";
+        description = "Summer Camo and AK-74's";
+        languages[] = {{"east", 100}};
     };
 
     class MNP_DPR_AUT {
         gear = "AK74_DPR";
         uniform = "DPR_AUT";
-        languages[] = {
-            {"east", 100}
-        };
+        name = "Donetsk People's Republic (Autumn)";
+        description = "Autumn Camo and AK-74's";
+        languages[] = {{"east", 100}};
     };
 
     class IND_F {
         gear = "Mk20_AAF_WD";
         uniform = "AAF_WD";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "AAF Woodland";
+        description = "Woodland Camo with Mk20's";
+        languages[] = {{"guer", 100}};
     };
 
     class AAF_D {
         gear = "Mk20_AAF_DE";
         uniform = "AAF_DE";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "AAF Desert";
+        description = "Desert Camo with Mk20's";
+        languages[] = {{"guer", 100}};
     };
 
     class MNP_MIL_WD {
         gear = "MIX_LVM";
         uniform = "LVM_WD";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "Local Militia (Woodland)";
+        description = "Woodland Militia with Mixed Weapons";
+        languages[] = {{"guer", 100}};
     };
 
     class MNP_MIL_DE {
         gear = "MIX_LVM";
         uniform = "LVM_DE";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "Local Militia (Desert)";
+        description = "Desert Militia with Mixed Weapons";
+        languages[] = {{"guer", 100}};
     };
 
     class MNP_NPA_WD {
         gear = "AKM_NPA";
         uniform = "NPA_WD";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "National People's Army (Woodland)";
+        description = "Woodland DPM Militia with AKM's";
+        languages[] = {{"guer", 100}};
     };
 
     class MNP_NPA_DE {
         gear = "AKM_NPA";
         uniform = "NPA_DE";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "National People's Army (Desert)";
+        description = "Desert DPM Militia with AKM's";
+        languages[] = {{"guer", 100}};
     };
 
     class MNP_ASA {
         gear = "AR15_ASA";
         uniform = "ASA";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "Allied States of America";
+        description = "US Milita with AR-15's";
+        languages[] = {{"guer", 100}};
     };
 
     class MNP_ARC {
         gear = "FAL_ARC";
         uniform = "ARC";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "Altis Revolutionary Council";
+        description = "Olive Drab Rebels with FAL";
+        languages[] = {{"guer", 100}};
     };
 
     class MNP_IRE_WD {
         gear = "AUGA3_IRE";
         uniform = "IRE_WD";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "Irish Army (Woodland)";
+        description = "Woodland Uniforms and AUG A3's";
+        languages[] = {{"guer", 100}};
     };
 
     class MNP_IRE_DE {
         gear = "AUGA3_IRE";
         uniform = "IRE_DE";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "Irish Army (Desert)";
+        description = "Desert Uniforms and AUG A3's";
+        languages[] = {{"guer", 100}};
     };
 
     class MNP_NZ {
         gear = "AUGA3_NZ";
         uniform = "NZ";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "New Zealand Army";
+        description = "Universal Uniforms and AUG A3's";
+        languages[] = {{"guer", 100}};
     };
 
     class MNP_FIN_WD {
         gear = "VZ58_FIN";
         uniform = "FIN_WD";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "Finnish Army (Woodland)";
+        description = "Woodland Uniforms and Vz58's";
+        languages[] = {{"guer", 100}};
     };
 
     class MNP_FIN_SN {
         gear = "VZ58_FIN";
         uniform = "FIN_SN";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "Finnish Army (Snow)";
+        description = "Snow Uniforms and Vz58's";
+        languages[] = {{"guer", 100}};
     };
 
     class MNP_NOR_WD {
         gear = "G3KA4_NOR";
         uniform = "NOR_WD";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "Norwegian Army (Woodland)";
+        description = "Woodland Uniforms and G3KA4's";
+        languages[] = {{"guer", 100}};
     };
 
-    class MNP_NOR_DE{
+    class MNP_NOR_DE {
         gear = "G3KA4_NOR";
         uniform = "NOR_DE";
-        languages[] = {
-            {"guer", 100}
-        };
+        name = "Norwegian Army (Desert)";
+        description = "Desert Uniforms and G3KA4's";
+        languages[] = {{"guer", 100}};
     };
+
 };
