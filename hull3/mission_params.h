7ca59d936cc3e566a659a189c479324ef2d7b499
Cyruz143 <root.cyruz@gmail.com>
2018-10-16 11:28:59 +0100

    Removing Humidy from mission params

--- hull3/mission_params.h
+++ hull3/mission_params.h
@@ -24,17 +24,17 @@ class MissionParams {
         {0.4, 0, 0} // Heavy
     };
 
-    // Overcast, Rain, Rainbow, Lightnings, Wind Strength, Wind Gusts, Waves, Humidity
+    // Overcast, Rain, Rainbow, Lightnings, Wind Strength, Wind Gusts, Waves
     weather[] = {
-        {-1}, // Random
-        {0,     0,      0,      0,      0,      0,      0,      0}, // Clear (Calm)
-        {0.01,  0,      0,      0,      0.25,   0.5,    0.25,   0.2}, // Clear (Light Winds)
-        {0.01,  0,      0,      0,      0.50,   0.75,   0.75,   0.2}, // Clear (Stong Winds)
-        {0.48,  0,      0,      0,      0,      0,      0.1,    0.2}, // Overcast (Calm)
-        {0.48,  0,      0,      0,      0.25,   0.5,    0.25,   0.2}, // Overcast (Light Winds)
-        {0.48,  0,      0,      0,      0.50,   0.75,   0.75,   0.2}, // Overcast (Strong Winds)
-        {1,     1,      0,      0,      0.25,   0.5,    0.75,   0.9}, // Rain (Light Winds)
-        {1,     1,      0,      0,      0.50,   0.75,   0.75,   0.9}, // Rain (Strong Winds)
-        {1,     1,      0,      1,      0.75,     1,      1,      1}, // Storm
+        {-1},                                   // Random
+        {0, 0, 0, 0, 0, 0,  0},                 // Clear (Calm)
+        {0.01, 0, 0, 0, 0.25, 0.5, 0.25},       // Clear (Light Winds)
+        {0.01, 0, 0, 0, 0.50, 0.75, 0.75},      // Clear (Stong Winds)
+        {0.48, 0, 0, 0, 0,  0, 0.1},            // Overcast (Calm)
+        {0.48, 0, 0, 0, 0.25, 0.5, 0.25},       // Overcast (Light Winds)
+        {0.48, 0, 0, 0, 0.50, 0.75, 0.75},      // Overcast (Strong Winds)
+        {1, 1, 0, 0, 0.25, 0.5, 0.75},          // Rain (Light Winds)
+        {1, 1, 0, 0, 0.50, 0.75, 0.75},         // Rain (Strong Winds)
+        {1, 1, 0, 1, 0.75, 1, 1}                // Storm
     };
 };
