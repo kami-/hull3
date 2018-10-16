a7de7749584697f2f9a5754a70c2693e963a1f47
Cyruz143 <root.cyruz@gmail.com>
2018-10-16 14:02:18 +0100

    Yet further format messing

--- hull3/mission_params.h
+++ hull3/mission_params.h
@@ -26,19 +26,19 @@ class MissionParams {
 
     // Overcast, Rain, Rainbow, Lightnings, Wind Strength, Wind Gusts, Waves
     weather[] = {
-        {-1},                                                   // Random
+        {-1},                                                      // Random
         
-        {0,    0,    0,    0,    0,    0,    0},                // Clear (Calm)
-        {0,    0,    0,    0,    0.25,    0.25,    0.25},       // Clear (Light Winds)
-        {0,    0,    0,    0,    0.50,    0.50,    0.50},       // Clear (Stong Winds)
+        {0,       0,    0,    0,       0,       0,       0},       // Clear (Calm)
+        {0,       0,    0,    0,       0.25,    0.25,    0.25},    // Clear (Light Winds)
+        {0,       0,    0,    0,       0.50,    0.50,    0.50},    // Clear (Stong Winds)
         
-        {0.48,    0,    0,    0,    0,    0,    0},             // Overcast (Calm)
-        {0.48,    0,    0,    0,    0.25,    0.25,    0.25},    // Overcast (Light Winds)
-        {0.48,    0,    0,    0,    0.50,    0.50,    0.50},    // Overcast (Strong Winds)
+        {0.48,    0,    0,    0,       0,       0,       0},       // Overcast (Calm)
+        {0.48,    0,    0,    0,       0.25,    0.25,    0.25},    // Overcast (Light Winds)
+        {0.48,    0,    0,    0,       0.50,    0.50,    0.50},    // Overcast (Strong Winds)
         
-        {1,    1,    0,    0.25,    0.25,    0.25,    0.25},    // Rain (Light Winds)
-        {1,    1,    0,    0.50,    0.50,    0.50,    0.50},    // Rain (Strong Winds)
+        {1,       1,    0,    0.25,    0.25,    0.25,    0.25},    // Rain (Light Winds)
+        {1,       1,    0,    0.50,    0.50,    0.50,    0.50},    // Rain (Strong Winds)
         
-        {1,    1,    0,    1,    0.75,    1,    1}              // Storm
+        {1,       1,    0,    1,       0.75,    1,       1}        // Storm
     };
 };
