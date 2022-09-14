3b1b56acbefb6b1973a4fed4477603e99b05f497
Cyruz143 <root.cyruz@gmail.com>
2022-09-14 14:40:03 +0100

    Nicer default now textures

--- hull3/mission_params.h
+++ hull3/mission_params.h
@@ -47,27 +47,22 @@ class MissionParams {
 
     // See https://community.bistudio.com/wiki/setRain for details
     snow[] = {
-        "a3\data_f\rainnormal_ca.paa",  // rainDropTexture
-        1,      // texDropCount
-        0.01,   // minRainDensity
-        15,     // effectRadius
-        0.1,    // windCoef
-        2,      // dropSpeed
-        0.5,    // rndSpeed
-        0.5,    // rndDir
-        0.02,   // dropWidth
-        0.02,   // dropHeight
-        {
-            0.1,    // R
-            0.1,    // G
-            0.1,    // B
-            1,      // A
-        },
-        0.1,    // lumSunFront
-        0.1,    // lumSunBack
-        5.5,    // refractCoef
-        0.3,    // refractSaturation
-        true,   // snow
-        false   // dropColorStrong
+        "a3\data_f\snowflake4_ca.paa", // rainDropTexture
+        4, // texDropCount
+        0.01, // minRainDensity
+        25, // effectRadius
+        0.05, // windCoef
+        2.5, // dropSpeed
+        0.5, // rndSpeed
+        0.5, // rndDir
+        0.07, // dropWidth
+        0.07, // dropHeight
+        [1, 1, 1, 0.5], // dropColor
+        0.0, // lumSunFront
+        0.2, // lumSunBack
+        0.5, // refractCoef
+        0.5, // refractSaturation
+        true, // snow
+        false // dropColorStrong
     };
 };
